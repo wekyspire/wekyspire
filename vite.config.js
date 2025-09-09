9ddff63a41dad53b02af68ab7665527496f50563
Hineven <hinevenwob@qq.com>
2025-09-09 15:16:46 +0800

    update github actions

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE || 'https://invalidurl.com/',
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => tag.startsWith('colored-')
+export default defineConfig(({mode}) => {
+  const root = process.cwd();
+  const viteEnv = loadEnv(mode, root);
+  return {
+    base: viteEnv.VITE_BASE || './',
+    plugins: [vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (tag) => tag.startsWith('colored-')
+        }
       }
+    })],
+    resolve: {
+      extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
+    },
+    server: {
+      host: 'localhost',
+      port: 5177
     }
-  })],
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
-  },
-  server: {
-    host: 'localhost',
-    port: 5177
-  }
-})
+}; })
