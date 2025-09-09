47590e0ba82f3732b91ab1e6dbb1f33ffee42505
Hineven <hinevenwob@qq.com>
2025-09-09 13:01:54 +0800

    Simplify base URL configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: (process.env.VITE_BASE || '/') + 'wekyspire/',
+  base: process.env.VITE_BASE || '/',
   plugins: [vue({
     template: {
       compilerOptions: {
